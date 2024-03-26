5068d2d54305d36b8256e550269a2a7b8750dc21
Fausto G. Cintra <professor@faustocintra.com.br>
2024-03-26 09:13:47 -0300

    (26/03) Exercícios 02 e 03

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home'
 import Exercicio01 from './exercicios/01'
 import Exercicio02 from './exercicios/02'
+import Exercicio03 from './exercicios/03'
 
 function App() {
   return (
@@ -12,12 +13,14 @@ function App() {
         <ul>
           <li> <Link to="/01">Exercício 01</Link> </li>
           <li> <Link to="/02">Exercício 02</Link> </li>
+          <li> <Link to="/03">Exercício 03</Link> </li>
         </ul>
         <hr />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/01" element={<Exercicio01 />} />
           <Route path="/02" element={<Exercicio02 />} />
+          <Route path="/03" element={<Exercicio03 />} />
         </Routes>
       </BrowserRouter>
     </>
--- react-hooks/src/exercicios/02.jsx
+++ react-hooks/src/exercicios/02.jsx
@@ -3,11 +3,31 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 inicialize o estado como o valor do localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(
+    // Quando uma função é passada como parâmetro no
+    // valor inicial do useState(), a atribuição do valor
+    // inicial acontece apenas durante a fase de montagem
+    // do compomente (que acontece apenas uma vez).
+    // Esse recurso é chamado LAZY INITIALIZER
+    // () => {
+    //   console.count('Leu localStorage')
+    //   return window.localStorage.getItem('name') ?? initialName
+    // }
+    () => window.localStorage.getItem('name') ?? initialName
+  )
+
+  const [asterisco, setAsterisco] = React.useState('*')
 
   // 🐨 Aqui é onde usamos `React.useEffect`.
   // A função deve armazenar `name` no localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+    console.count('Atualizou')
+  }, [name]) // VETOR DE DEPENDÊNCIAS
+  // O vetor de dependências permite "filtrar" quais variáveis
+  // de estado serão "vigiadas" em busca de alterações para 
+  // executar o efeito colateral
 
   function handleChange(event) {
     setName(event.target.value)
@@ -15,7 +35,9 @@ function Greeting({initialName = ''}) {
   return (
     <div>
       <form>
-        <label htmlFor="name">Name: </label>
+        <label htmlFor="name" onClick={() => setAsterisco(asterisco + '*')}>
+          Name: {asterisco} 
+        </label>
         <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Olá {name}</strong> : 'Por favor, informe seu nome'}
