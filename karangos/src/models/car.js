6dc3dd5b15aa195656d82385064fe44ac96ad020
Rafael Cesar <rafael.hc2610@gmail.com>
2024-05-23 10:21:57 -0300

    (23/05) Validação de veículos com Zod

---
+++ karangos/src/models/car.js
@@ -0,0 +1,42 @@
+import { z } from 'zod'
+
+const maxYearManufacture = new Date()   
+DateActual.setFullYear(DateActual.getFullYear())
+
+const minYearManufacture = 1960
+
+const Car = z.object({
+
+    brand:
+        z.string()
+        .max(25, { message: 'A Marca pode ter, no máximo, 25 caracteres' }),
+
+    model:
+        z.string()
+        .max(25, { message: 'O Modelo pode ter, no máximo, 25 caracteres' }),
+
+    color:
+        z.string()
+        .max(12, { message: 'A cor pode ter, no máximo, 12 caracteres' }),
+
+    year_manufacture:
+        
+        z.coerce.number()
+        .min(minYearManufacture, { message: 'O ano de fabricação está muito no passado' })
+        .max(maxYearManufacture, { message: 'O ano de fabricação deve ser maxímo deste ano' }),
+
+    plates:
+        z.string()
+        .length(8, { message: 'A placa deve ter, exatamente, 8 caracteres' }),
+
+    selling_price:
+        z.number()
+        .min(1000, { message: 'O valor de venda deve ser, no minimo, R$ 1000,00 ' })
+        .max(5000000,{ message: 'O valor de venda deve ser, no máximo, R$ 5.000.000,00'}),
+
+    imported:
+        z.boolean({ message: 'Esse campo deve ser preenchido apenas com true ou false' }),
+
+})
+
+export default Car
