afc01ca9ca67e731b3fa0b5ad0d65f0efbea7e46
Cássio Cintra <cassiocr2012@hotmail.com>
2024-06-04 09:38:57 -0300

    (04/06) Cadastro de Veículos

--- karangos/src/models/car.js
+++ karangos/src/models/car.js
@@ -1,5 +1,11 @@
 import { z } from 'zod'
 
+const maxYear = new Date()   // Data de hoje
+maxYear.setFullYear(maxYear.getFullYear())
+
+const minYear = new Date(1960, 0, 1)
+minYear.setFullYear(minYear.getFullYear())
+
 const Car = z.object({
     brand:
         z.string().max(25, {message: 'Máximo de 25 caracteres'}),
@@ -8,14 +14,20 @@ const Car = z.object({
     color:
         z.string().max(12, {message: 'Máximo de 12 caracteres'}),
     year_manufacture:
-        z.number().min(1960).max(2024),
+        z.coerce.date()
+        .min(minYear,{message: 'O ano mínimo é 1960'})
+        .max(maxYear,{message: 'O ano máximo é 2024'}),
     imported:
-        z.boolean({
+        z.coerce.boolean({
             required_error: 'Resposta obrigatória',
             invalid_type_error: 'Resposta inválida',
         }),
     plates:
         z.string().length(8, {message: 'Deve ter 8 caracteres'}),
     selling_price:
-        z.number().min(1000).max(5000000),
+        z.coerce.number()
+        .min(1000, {message: 'Valor mínimo de R$1.000,00'})
+        .max(5000000, {message: 'Valor máximo de R$5.000.000,00'}),
 })
+
+export default Car
